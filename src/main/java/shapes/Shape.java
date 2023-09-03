d14fb25d649669d699afda20cc678417f2531146
AlexAksioma <117904275+AlexAksioma@users.noreply.github.com>
2023-09-03 14:33:50 +0300

    added get, set class shape

---
+++ src/main/java/shapes/Shape.java
@@ -0,0 +1,38 @@
+package shapes;
+
+public abstract class Shape {
+    protected int x, y;
+    private String colour;
+
+    private boolean visible;
+
+    public Shape(int x, int y, String color, boolean visible) {
+        this.x = x;
+        this.y = y;
+        setColor(color);
+        setVisible(visible);
+    }
+
+    public String getColor() {
+        return colour;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setColor(String color) {
+        if (colour == null)
+            this.colour = "white";
+        else
+            this.colour = color;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public abstract double getArea();
+
+    public abstract double gerPerimeter();
+}
